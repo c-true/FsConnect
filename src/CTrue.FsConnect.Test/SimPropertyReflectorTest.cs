8278f9ede9f972013b29bb49549b9b31426e3051
Timian Heber <timian.heber@c-true.com>
2021-04-04 23:23:34 +0200

    Work on reflection

---
+++ src/CTrue.FsConnect.Test/SimPropertyReflectorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Microsoft.FlightSimulator.SimConnect;
+using NUnit.Framework;
+
+namespace CTrue.FsConnect.Test
+{
+    [TestFixture]
+    public class SimPropertyReflectorTest
+    {
+        [Test]
+        public void Test()
+        {
+            SimPropertyReflector reflector = new SimPropertyReflector();
+
+            List<SimProperty> list = reflector.GetSimProperties<SimConnectObject>();
+
+            Assert.That(list, Is.Not.Null);
+            Assert.That(list.Count, Is.EqualTo(2));
+
+            Assert.That(list[0].Name, Is.EqualTo("A256CharString"));
+            Assert.That(list[0].Unit, Is.EqualTo(""));
+            Assert.That(list[0].DataType, Is.EqualTo(SIMCONNECT_DATATYPE.STRING256));
+
+            Assert.That(list[1].Name, Is.EqualTo("ADouble"));
+            Assert.That(list[1].Unit, Is.EqualTo("Feet"));
+            Assert.That(list[1].DataType, Is.EqualTo(SIMCONNECT_DATATYPE.FLOAT64));
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct SimConnectObject
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public String A256CharString;
+        
+        [SimProperty(Unit = "Feet")]
+        public double ADouble;
+    };
+}
---
+++ src/CTrue.FsConnect/SimPropertyReflector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.FlightSimulator.SimConnect;
+
+namespace CTrue.FsConnect
+{
+    public class SimPropertyReflector
+    {
+        public List<SimProperty> GetSimProperties<T>() where T : struct
+        {
+            List<SimProperty> simProperties = new List<SimProperty>();
+
+            Type type = typeof(T);
+
+            var fields = type.GetFields();
+
+            foreach (var field in fields)
+            {
+                SimProperty simProperty = new SimProperty();
+
+                SimPropertyAttribute attr = field.GetCustomAttribute<SimPropertyAttribute>();
+
+                if (!string.IsNullOrEmpty(attr?.Name))
+                {
+                    simProperty.Name = attr.Name;
+                }
+                else
+                    simProperty.Name = field.Name;
+
+                if (attr != null && attr.DataType != SIMCONNECT_DATATYPE.INVALID)
+                {
+                    simProperty.DataType = attr.DataType;
+                }
+                else
+                {
+                    if (field.FieldType == typeof(string))
+                        simProperty.DataType = SIMCONNECT_DATATYPE.STRING256;
+                    else
+                        simProperty.DataType = SIMCONNECT_DATATYPE.FLOAT64;
+                }
+
+                if (!string.IsNullOrEmpty(attr?.Unit))
+                {
+                    simProperty.Unit = attr.Unit;
+                }
+                else
+                    simProperty.Unit = "";
+
+                simProperties.Add(simProperty);
+            }
+
+            return simProperties;
+        }
+    }
+}
